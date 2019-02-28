7e00f21052e70d28379ee745c4a8127a97ef6aea
twinzxr1 <alexandrebouchard@hotmail.fr>
2019-02-28 10:53:45 +0100

    refactoring var env

--- main.js
+++ main.js
@@ -1,36 +1,35 @@
 const Discord = require('discord.io');
 const fetch = require('node-fetch');
 
-// On récupère des functions qui écrivent dans le tchat
 const tools = require('./tools.js');
 
-// Si on passe le token en variable d'environnement
-if (process.env.TOKEN){
-    auth = {
-        "token" : process.env.TOKEN
-    }
+// On récupère les variables d'environnement
+if (process.env.DISCORD_TOKEN && process.env.CHAN_ID){
+    DISCORD_TOKEN = process.env.DISCORD_TOKEN
+    CHAN_ID = process.env.CHAN_ID
 }
-// Sinon récupère le token d'accès stocké dans le fichier
-else {
-    auth = require('./auth.json');
-}
+// elles sont obligatoires
+else return
+
+
 
 // Initialize Discord Bot
 const bot = new Discord.Client({
-   token: auth.token,
+   token: DISCORD_TOKEN,
    autorun: true
 });
 
 bot.on('ready', async function (evt) {
     bot.sendMessage({
-        to: '550105564336750602',
+        to: CHAN_ID,
         message: `Bot connecté`
     });
 });
 
 
 bot.on('message', async function (user, userID, channelID, message, evt) {
-    //console.log('test', channelID, message);
+    // on écoute uniquement le channel que l'on à configurer dans les variables d'environnements
+    if (channelID != CHAN_ID) return
 
     if (message.substring(0, 1) == '!') {
         args = message.substring(1).split(' ');
